11fd5bac996f8bcd7059a37bc0cfdda87685ff4c
hdinjos <hdinjos@gmail.com>
2021-06-27 22:55:29 +0700

    binding data input with state in module vuex formInput

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -3,30 +3,104 @@
     <div class="row">
       <div class="col-12 mb-2">
         <div>Title</div>
-        <input type="text" class="form-control" placeholder="Hello My Prince" />
+        <input
+          v-model="title"
+          type="text"
+          class="form-control"
+          placeholder="Hello My Prince"
+        />
       </div>
       <div class="col-12 mb-2">
         <div>Author</div>
-        <input type="text" class="form-control" placeholder="Ahimsa Azaleav" />
+        <input
+          v-model="author"
+          type="text"
+          class="form-control"
+          placeholder="Ahimsa Azaleav"
+        />
       </div>
       <div class="col-12 mb-2">
         <div>Publisher</div>
-        <input type="text" class="form-control" placeholder="Kompas Gramedia" />
+        <input
+          v-model="publisher"
+          type="text"
+          class="form-control"
+          placeholder="Kompas Gramedia"
+        />
       </div>
       <div class="col-12 mb-2">
         <div>Publication Year</div>
-        <input type="text" class="form-control" placeholder="2021" />
+        <input
+          v-model="publicationYear"
+          type="text"
+          class="form-control"
+          placeholder="2021"
+        />
       </div>
       <div class="col-12 mb-2">
         <div>Print</div>
-        <input type="text" class="form-control" placeholder="III" />
+        <input
+          v-model="print"
+          type="text"
+          class="form-control"
+          placeholder="III"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    title: {
+      get() {
+        return this.$store.getters["formInput/getTitle"];
+      },
+      set(payload) {
+        this.$store.commit("formInput/updateTitle", payload);
+      },
+    },
+    author: {
+      get() {
+        return this.$store.getters["formInput/getAuthor"];
+      },
+      set(payload) {
+        this.$store.commit("formInput/updateAuthor", payload);
+      },
+    },
+    publisher: {
+      get() {
+        return this.$store.getters["formInput/getPublisher"];
+      },
+      set(payload) {
+        this.$store.commit("formInput/updatePublisher", payload);
+      },
+    },
+    publicationYear: {
+      get() {
+        return this.$store.getters["formInput/getPublicationYear"];
+      },
+      set(payload) {
+        this.$store.commit("formInput/updatePublicationYear", payload);
+      },
+    },
+    print: {
+      get() {
+        return this.$store.getters["formInput/getPrint"];
+      },
+      set(payload) {
+        this.$store.commit("formInput/updatePrint", payload);
+      },
+    },
+  },
+  // updated() {
+  //   console.log(this.$store.getters["formInput/getPrint"]);
+  // },
+  // mounted() {
+  //   console.log(this.$store.getters["formInput/getPrint"]);
+  // },
+};
 </script>
 
 <style>
