47bba5b2218c8a22352da3bd169a412abe4e184f
hdinjos <hdinjos@gmail.com>
2021-06-28 00:31:47 +0700

    make editBook mutation in global store to edit books

--- src/store/index.js
+++ src/store/index.js
@@ -26,6 +26,13 @@ export default new Vuex.Store({
     deleteBook(state) {
       state.books.splice(state.indexDelete, 1);
     },
+    editBook(state, payload) {
+      state.books[payload.index].title = payload.title;
+      state.books[payload.index].author = payload.author;
+      state.books[payload.index].publisher = payload.publisher;
+      state.books[payload.index].publicationYear = payload.publicationYear;
+      state.books[payload.index].print = payload.print;
+    },
   },
   actions: {},
   modules: {
