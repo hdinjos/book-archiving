5bd7b9e72ac79166d149e97d9c4ce8f9d87b7834
hdinjos <hdinjos@gmail.com>
2021-06-27 18:05:04 +0700

    add button add books

---
+++ src/pages/AddBook/AddButton/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="d-flex justify-content-end">
+    <button class="btn btn-primary end">Add Book</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
