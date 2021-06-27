bd3b42aff96c34bd7f2924d2ba6f02db8ba7d283
hdinjos <hdinjos@gmail.com>
2021-06-27 21:42:48 +0700

    add modal edit and delete book

---
+++ src/pages/Home/ModalDeleteBook/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="modal fade"
+    id="modalDelete"
+    tabindex="-1"
+    aria-labelledby="modalDeleteLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title text-warning" id="modalDeleteLabel">
+            Warning!
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <h4>Do you want to delete this book?</h4>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Back
+          </button>
+          <button data-bs-dismiss="modal" type="button" class="btn btn-primary">
+            Yes
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/pages/Home/ModalEditBook/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Edit Book</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <FormInput />
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Back
+          </button>
+          <button data-bs-dismiss="modal" type="button" class="btn btn-primary">
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FormInput from "@/components/FormInput";
+export default {
+  components: {
+    FormInput,
+  },
+};
+</script>
+
+<style>
+</style>
