da47b0d444148e1baf6d91b56edccbbd965b10bd
hdinjos <hdinjos@gmail.com>
2021-06-27 20:28:05 +0700

    add form input component

---
+++ src/components/FormInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card-body">
+    <div class="row">
+      <div class="col-12 mb-2">
+        <div>Title</div>
+        <input type="text" class="form-control" placeholder="Hello My Prince" />
+      </div>
+      <div class="col-12 mb-2">
+        <div>Author</div>
+        <input type="text" class="form-control" placeholder="Ahimsa Azaleav" />
+      </div>
+      <div class="col-12 mb-2">
+        <div>Publisher</div>
+        <input type="text" class="form-control" placeholder="Kompas Gramedia" />
+      </div>
+      <div class="col-12 mb-2">
+        <div>Publication Year</div>
+        <input type="text" class="form-control" placeholder="2021" />
+      </div>
+      <div class="col-12 mb-2">
+        <div>Print</div>
+        <input type="text" class="form-control" placeholder="III" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
