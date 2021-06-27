cb399ad1ff0c1c9a1ba2af77b407209c638dc276
hdinjos <hdinjos@gmail.com>
2021-06-27 20:29:13 +0700

    use component form input to add book page

--- src/pages/AddBook/index.vue
+++ src/pages/AddBook/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>this add books</div>
+  <div>
+    <h1 class="text-center">Add your book now</h1>
+    <FormInput />
+  </div>
 </template>
 
 <script>
-export default {};
+import FormInput from "@/components/FormInput";
+export default {
+  components: {
+    FormInput,
+  },
+};
 </script>
 
 <style>
